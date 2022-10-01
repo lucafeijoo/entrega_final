d6e8a0544ee97ffba34b9d964870b1f1a1e95794
Luca Garavelli <lucagaravelli10@gmail.com>
2022-10-01 17:59:01 -0300

    Hecho el static y la plantilla base

--- my_app/views.py
+++ my_app/views.py
@@ -4,7 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
 
+<<<<<<< HEAD
         return HttpResponse("vista inicio")
+=======
+        return render(request, "my_app/base.html")
+>>>>>>> f78d110 (Hecho el static y la plantilla base)
 
 def nosotros(request):
 

75b907027c48098c2dc8cf0059dd9fc753b18e90
Luca Garavelli <lucagaravelli10@gmail.com>
2022-10-04 21:49:43 -0300

    Hecho el test

--- my_app/tests.py
+++ my_app/tests.py
@@ -1,3 +1,22 @@
+import random
+import string
 from django.test import TestCase
+from my_app.models import Blog
 
-# Create your tests here.
+
+class BlogTestCase(TestCase):
+
+        def test_creacion_blog(self):
+                #Test: Comprobar puedo crear un blog con un titulo, subtitulo y cuerpo con letras random. Se espera que no ocurra errores.
+                lista_letras_titulo = [random.choice(string.ascii_letters + string.digits) for _ in range(20)]
+                lista_letras_subtitulo = [random.choice(string.ascii_letters + string.digits) for _ in range(30)]
+                lista_letras_cuerpo = [random.choice(string.ascii_letters + string.digits) for _ in range(30)]
+                titulo_prueba = "".join(lista_letras_titulo)
+                subtitulo_prueba = "".join(lista_letras_subtitulo)
+                cuerpo_prueba = "".join(lista_letras_cuerpo)
+                blog_1 = Blog.objects.create(titulo=titulo_prueba,subtitulo=subtitulo_prueba,cuerpo=cuerpo_prueba)
+
+                self.assertIsNotNone(blog_1.id)
+                self.assertEqual(blog_1.titulo, titulo_prueba)
+                self.assertEqual(blog_1.subtitulo, subtitulo_prueba)
+                self.assertEqual(blog_1.cuerpo, cuerpo_prueba)
